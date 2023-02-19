3a32b72dd5986dfd9f5f2341142bc6ccb5998381
dnadawa <dulajnadawa@gmail.com>
2023-02-19 10:39:23 +0530

    init

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const multer = require('multer');
+const AWS = require('aws-sdk');
+require('dotenv').config()
+
+const app = express();
+
+const s3 = new AWS.S3({
+    accessKeyId: process.env.ACCESS_KEY,
+    secretAccessKey: process.env.SECRET_KEY
+});
+
+const storage = multer.memoryStorage({
+    destination: function (req, file, callback) {
+        callback(null, '');
+    }
+});
+
+const upload = multer({storage}).single("image");
+
+
+app.post('/upload', upload, (req, res) => {
+    console.log("starting upload of " + req.file.originalname);
+    const params = {
+        Bucket: process.env.BUCKET,
+        Key: req.file.originalname,
+        Body: req.file.buffer
+    };
+
+    s3.upload(params,  (err, data) => {
+        if(err){
+            res.status(500).send(err);
+        }
+
+        res.status(200).send({
+            'status': "Success"
+        });
+    });
+});
+
+
+app.listen(3000, () => {
+    console.log("Server started!");
+});
